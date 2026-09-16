cc6a42f3182cedcd28447c0cb48df259830350ea
Harbey Cely <hecm-09@hotmail.com>
2026-09-16 08:42:22 -0500

    Teoria y ejercicio tablas de verdad en word

--- Python-Sena/Clase1.py
+++ Python-Sena/Clase1.py
@@ -8,12 +8,12 @@
 
 
 
-# Programa para calcular el IVA de un articulo si su precio es >= 100.000
-compra = float(input("Ingrese valor de la compra: "))
-if compra >= 100000:
-    iva = compra * 0.19
-    print("El Iva es: ",iva)
-    total = compra+iva
-    print("El total es",total)
-else:
-    print("No paga Iva")
+# # Programa para calcular el IVA de un articulo si su precio es >= 100.000
+# compra = float(input("Ingrese valor de la compra: "))
+# if compra >= 100000:
+#     iva = compra * 0.19
+#     print("El Iva es: ",iva)
+#     total = compra+iva
+#     print("El total es",total)
+# else:
+#     print("No paga Iva")
